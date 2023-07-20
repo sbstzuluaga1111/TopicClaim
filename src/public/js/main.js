d971d92efa6c6536199c0fd7ace727c3068bcf88
sbstzuluaga1111 <sbstzuluagajob@gmail.com>
2023-07-20 15:51:36 -0500

    V3

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,1 +1,23 @@
-io();
+$(function() {
+    const socket = io();
+
+const $messageForm = $('#message-form');
+const $messageBox = $('#message');
+const $chat = $('#chat');
+
+
+$messageForm.submit( e =>{
+
+e.preventDefault();
+
+socket.emit('send message', $messageBox.val());
+$messageBox.val('');
+
+});
+
+socket.on('new message', function (data) {
+    $chat.append(data + '<br/>');
+});
+
+
+})
--- src/sockets.js
+++ src/sockets.js
@@ -1,5 +1,10 @@
 module.exports = function (io){
     io.on('connection', socket => {
         console.log('conexión exitosa');
+
+        socket.on('send message', function (data){
+            io.sockets.emit('new message', data);
+        });
+
     });
 }

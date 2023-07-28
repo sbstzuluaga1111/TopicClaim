4a251ef3d479884645fb0fccba74ce7b38e61f12
sbstzuluaga1111 <sbstzuluagajob@gmail.com>
2023-07-28 07:50:03 -0500

    V11

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -7,6 +7,27 @@ const $chat = $('#chat');
 
 
 
+const $registroForm = $('#Regis');
+const $error = $('#errorRegis');
+
+const $nameUser = $('#name_field');
+const $emailUser = $('#email_field');
+const $password = $('#password_field');
+
+const $listado = $('#userName')
+
+
+
+$registroForm.submit(e => {
+socket.emit('newUser', $nameUser.val(), data => {
+
+    }); 
+});
+
+
+
+
+
 $messageForm.submit( e =>{
 
 e.preventDefault();

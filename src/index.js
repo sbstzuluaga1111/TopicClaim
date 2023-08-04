1fba17c57bd7f0dd05a25e3e66495636d35774fa
sbstzuluaga1111 <sbstzuluagajob@gmail.com>
2023-08-04 13:27:52 -0500

    V14

--- src/index.js
+++ src/index.js
@@ -1,19 +1,33 @@
-const http = require('http');
-const path = require('path');
+
+// Iniciar Servidor a secas// 
 const express = require('express');
-const socketio = require('socket.io');
 
 const app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+server.listen(app.get('port'), () => {
+    console.log('El servidor se inició en el puerto ',app.get('port'));
+});
+// Iniciar Servidor a secas//
+
+
+
+const http = require('http');
+const path = require('path');
+
+const socketio = require('socket.io');
+
+
 const server = http.createServer(app);
 const io = socketio(server);
 
-app.set('port', process.env.PORT || 3000);
+
 
 require('./sockets')(io);
 
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-server.listen(app.get('port'), () => {
-    console.log('El servidor se inició en el puerto ',app.get('port'));
-});
+
+
---
+++ src/routes/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+router('/', (req, res, next) => {
+
+});
+
+module.exports = router;
